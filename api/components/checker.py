37c1483dbca83d00f8058457560a179c84dd7e51
Nathan Mainka Soares <50429336+nathanmsoares@users.noreply.github.com>
2021-07-29 21:09:51 -0300

    Revert "Revert "finished unit tests, README, Logging, correcting some bugs""
    This reverts commit 3bbac9fa8c1cd7a6509fdbf5957dec00e3a1bb1e.

--- api/components/checker.py
+++ api/components/checker.py
@@ -1,6 +1,7 @@
 from typing import List, OrderedDict
 import requests
 from rest_framework.exceptions import ParseError
+from .logger import logger
 
 
 class PackageChecker():
@@ -8,27 +9,31 @@ class PackageChecker():
     @staticmethod
     def checking_packages(package_names: List) -> \
             List[OrderedDict]:
+        logger.debug(
+            "[Checker] Checking if theses packages and their versions exist" +
+            f"{package_names}")
         error_case_dict = {"error": "One or more packages doesn't exist"}
         checked_packages: List[OrderedDict] = []
         for package in package_names:
-            print(checked_packages)
             response = requests.get(
                 f"https://pypi.org/pypi/{package['name']}/json")
             if response.status_code != 200:
+                logger.debug(f"[Checker] {package['name']} does not exist")
                 raise ParseError(error_case_dict)
             else:
                 package_versions = list(response.json()['releases'].keys())
                 if package.get("version") is None:
                     package['version'] = package_versions[-1]
-                    print(package_versions)
-                    print(type(package_versions))
                     checked_packages.append(package)
                     continue
                 elif package['version'] not in package_versions:
+                    logger.debug(
+                        f"[Checker] version {package['version']} of" +
+                        f"{package['name']} does not exist")
                     raise ParseError(error_case_dict)
                 else:
+                    logger.debug(f"[Checker] Adding {package} to the list")
                     checked_packages.append(package)
-        print(checked_packages)
-        print(response.status_code)
-        print(type(response))
+        logger.debug(
+            f"[Checker] Returning checked packages {checked_packages}")
         return checked_packages
--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,6 @@
 from api.models import Project
 from rest_framework import status
 from rest_framework.test import APITestCase
-import json
-import requests
 
 
 class AccountTests(APITestCase):
@@ -38,32 +36,25 @@ class AccountTests(APITestCase):
         self.assertEqual(Project.objects.count(), 1)
         self.assertEqual(Project.objects.get().name, 'titan')
         response = self.client.get(url, format='json')
-        data1 = {
-            "name": "titan2",
-            "packages": [
-                {
-                    "name": "Django",
-                    "version": "1.0.4"
-                },
-                {
-                    "name": "graphene",
-                    "version": "2.0"
-                },
-                {
-                    "name": "numpy",
-                    "version": "0.9.6"
-                },
-                {
-                    "name": "tornado",
-                    "version": "0.2"
-                }
-            ]
-        }
         self.assertDictEqual(response.json()[0], data)
-        response = self.client.patch(url+"titan", {'packages':
-                                                   [{"name": "Django",
-                                                     "version": "1.0.2"}]},
-                                     format='json')
+        response = self.client.post(url, {
+            "name": "titan",
+            "packages": [
+                {"name": "pypypypypypypypypypypy"},
+                {"name": "graphene", "version": "1900"}
+            ]
+        }, format='json')
         self.assertEqual(Project.objects.count(), 1)
-        response = self.client.get('http://127.0.0.1:8000/api/projects/titan')
-        print(response.content)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.json(), {
+                         'name': ['project with this name already exists.']})
+        response = self.client.post(url, {
+            "name": "titan1",
+            "packages": [
+                {"name": "pypypypypypypypypypypy"},
+                {"name": "graphene", "version": "1900"}
+            ]
+        }, format='json')
+        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(response.json(), {
+                         'error': "One or more packages doesn't exist"})
