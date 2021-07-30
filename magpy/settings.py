a8f5cb1315a37fbddf9fae2351f204400d2da8ab
Nathan Mainka Soares <nathan.soares.ns@gmail.com>
2021-07-30 02:45:10 +0000

    changed debug to False

--- magpy/settings.py
+++ magpy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o^6v#)1%sgi&jin)zm=p+=adcgjaqj+e+c$7c*8_8#7*z31uah'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
